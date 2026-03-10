4f4e95f70bbb95a9361accf38e66cbb1af07e72c
NHBINKS <nhbinks04@gmail.com>
2026-03-10 15:45:05 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const elements = document.querySelectorAll(".hidden");
+
+window.addEventListener("scroll", () => {
+
+elements.forEach(el => {
+
+const position = el.getBoundingClientRect().top;
+
+const screenPosition = window.innerHeight / 1.3;
+
+if(position < screenPosition){
+
+el.classList.add("show");
+
+}
+
+});
+
+});
